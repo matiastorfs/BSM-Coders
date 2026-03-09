7fb3046258862f4adc1424a0600b09cb5ac9bbac
Senne Smout <sennesmout@gmail.com>
2026-03-09 11:57:21 +0100

    werkend solo spel

--- guess-that-game/js/guess-that-game.js
+++ guess-that-game/js/guess-that-game.js
@@ -7,8 +7,3 @@ const oneVOne = document.getElementById("1v1");
 oneVOne.addEventListener("click", function () {
   window.location.href = "./guess-that-game-1v1.html";
 });
-
-const difficultyOne = document.getElementById("difficultyOne");
-difficultyOne.addEventListener("click", function () {
-  window.location.href = "./guess-that-game-easy.html";
-});
