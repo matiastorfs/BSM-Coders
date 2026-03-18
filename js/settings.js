0142ae081ac6afdbac932a0b77ef0d0a3c0a92d3
Senne Smout <sennesmout@gmail.com>
2026-03-18 20:33:53 +0100

    fixing settings mergev2

--- js/settings.js
+++ js/settings.js
@@ -3,3 +3,15 @@ const arrow = document.getElementById("arrow");
 arrow.addEventListener("click", function () {
   window.location.href = "./home.html";
 });
+
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach(tab => {
+  const header = tab.querySelector(".tab-header");
+
+  header.addEventListener("click", () => {
+    const content = tab.querySelector(".tab-content");
+
+    content.classList.toggle("show");
+  });
+});
