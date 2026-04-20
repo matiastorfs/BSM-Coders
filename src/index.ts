ea56081d2df33946a71c34d9ab7a305491e1fa13
Brahim Zarkani <s168877@ap.be>
2026-04-20 21:11:44 +0200

    Implement EJS and add navigation partial

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-// To run this in production, type the following commands:
+// To run this server in localhost, type the following commands:
+// npm install (one time only)
 // npm run build
 // npm start
 
@@ -8,8 +9,18 @@ import path from "path";
 const app = express();
 const root = process.cwd();
 
+app.set("view engine", "ejs");
+
 app.use(express.static(path.join(root, "public")));
 app.use(express.static(path.join(root, "dist")));
+
+app.get("/:page.html", (req, res) => {
+  res.render(req.params.page);
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
