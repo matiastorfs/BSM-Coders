8e54f1297fa0351ce2ad88e89efcf8a7c924c172
Senne Smout <sennesmout@gmail.com>
2026-05-06 09:20:12 +0000

    gamePlayFix

--- src/routers/guessthatgame.ts
+++ src/routers/guessthatgame.ts
@@ -2,21 +2,23 @@ import express from "express";
 import { getGames } from "../database";
 import { Game } from "../types";
 
-let games: Game[] = [];
-
 export default function guessRouter() {
   const router = express.Router();
 
   router.get("/", async (req, res) => {
-    games = await getGames();
+    const games: Game[] = await getGames();
 
     res.render("guess-that-game-homepagina", { games });
   });
 
   router.get("/1v1", async (req, res) => {
-    games = await getGames();
+    const games: Game[] = await getGames();
 
     res.render("guess-that-game-1v1", { games });
+  });
+
+  router.get("/play", async (req, res) => {
+    res.render("guess-that-game-play");
   });
 
   return router;
